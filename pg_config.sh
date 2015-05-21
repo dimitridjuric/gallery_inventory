7b9e7cc9d8d4863887010e5631113e76038223ee
dimitri djuric <dimitri.djuric@gmail.com>
2015-05-21 16:26:21 +0100

    Added a vagranfile, an postgres configuration script pg_config

---
+++ pg_config.sh
@@ -0,0 +1,14 @@
+
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb'
+su vagrant -c 'createdb gallerydbwithusers'
+su vagrant -c 'python /vagrant/gallerdatabase_setup.py'
+su vagrant -c 'psql -d gallerydbwithusers -f /vagrant/gallerydb.sql'
+
+
+
